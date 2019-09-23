e0d89d9bde3d71489766c39465658c54e1686175
Vincenzo Longobardi <vincylongo98@gmail.com>
2019-09-23 18:52:44 +0200

    fifth commit

---
+++ romanico/scripts/script2.js
@@ -0,0 +1,96 @@
+const Michel = {
+    title: 'Abbazia di Mont-Saint-Michel',
+    text: ''
+};
+const Antimo = {
+    title: 'Abbazia di Sant\'Antimo',
+    text: ''
+};
+const Moissac = {
+    title: 'Abbazia di San Pietro di Moissac',
+    text: 'Quella di Moissac, dedicata a San Pietro, è una chiesa abbaziale situata in Francia, nel comune di Moissac, e presenta nella parte inferiore in pietra con caratteristiche romaniche, mentre la parte superiore, in mattoni, ha caratteristiche gotiche. La particolarità di questa chiesa è il suo portale:<br>Il portale si presenta con una lunetta superiore realizzata in pietra, che presenta la visione di San Giovanni evangelista descritta nell\'Apocalisse. Al centro di questa lunetta troviamo il Cristo, con, al suo fianco, il tetramorfo. A circondare la scena, vi sono, ai lati e nella parte inferiore, ventiquattro vegliardi dotati di strumenti musicali e coppe, tutti rivolti verso il Cristo allo scopo di mettere in rilievo la scena centrale. Sull\'architrave, dunque nella parte inferiore della lunetta, vi sono le ruote infuocate, emblema del fuoco infernale. Il trumeau presenta invece sulla faccia delle figure di leoni addossati che si incrociano; sui lati vi sono il profeta Geremia e San Paolo, che si completano con Isaia e San Pietro sugli stipiti del portale.'
+};
+const Ambrogio = {
+    title: 'Basilica di Sant\'Ambrogio',
+    text: 'La basilica Milanese venne edificata tra il 379 e il 386 d.C. per volere del vescovo di Milano Ambrogio. La sua pianta presenta  tre navate absidale, senza transetto con quadriportico antistante; ulteriore caratteristica strutturarle è il principio delle campate, fondamentale per il Romanico, in cui la campata maggiore (considerata unità della navata centrale) equivale a quattro volte una campata minore delle navate laterali.  Lo stesso principio di moltiplicazione di una misura base viene applicato anche in modo orizzontale, in cui l\'altezza delle volta centrale corrisponde ai due livelli sovrapposti delle navate minori (dovuto alla presenza del matroneo). Leggendo la pianta si può distinguere l\'importanza degli elementi di sostegno, in cui sul piastro a sezione cruciforme insistono i costoloni delle due crociere laterali e invece sul pilastro a fascio gravano i costoloni delle due crociere laterali e quelli delle crociere maggiori.<br><br>All\'interno della Basilica articolata e dinamica sono evidenziate le linee d forza con l\'impiego del laterizio rosso dando così più importanza alla struttura portante. Ed è proprio grazie alla volta a crociera che si avvale di un misurato sistema di scarico sui pilastri di sostegno, che consente di alleggerire le murature aprendovi matronei praticabili.'
+};
+const Clemente = {
+    title: 'Basilica di San Clemente',
+    text: 'La Basilica di San Clemente, poco distante dal Colosseo, venne ricostruita nel XII secolo Sulle fondamenta di un edificio di origine paleocristiana. La pianta presenta un impianto a tre navate con preziose colonne Ioniche è un raffinato pavimento cromatesco, si conclude poi nell\'area absidale con uno splendido mosaico in cui ritroviamo una rielaborazione a girali da d\'acanto del Battistero Lateranense. In questo mosaico Oltre alle figure di Maria e Giovanni, che affiancano la croce e dei quattro padri della Chiesa, disposti sulla prima fascia di girali con indosso il saio dei monaci benedettini, il mosaico è popolato da quantità di figure umane e animali che alludono alla rigenerazione e al peccato. Questa chiesa viene considerata molto importante a Roma per gli interventi decorativi realizzati, che custodiscono una testimonianza rarissima (nelle iscrizioni degli affreschi è stata individuata una delle prime testimonianze scritte di lingua italiana volgare). Bisogna Inoltre notare che il mosaico propone una celebrazione del Nuovo Testamento che surclassa L\'Antico, con una gloriosa saltazione della Croce come fonte di vita e di speranza.'
+};
+const Venezia = {
+    title: 'Basilica di San Marco',
+    text: 'Venezia tramite il Mar Adriatico rimase per tutto il Medioevo in contatto con la cultura bizantina tant\'è che questa intenza influenza la si ritrova sia nell\'architettura che nelle decorazioni a mosaico a sfondo oro. Spesso le maestranze venivano direttamente da Costantinopoli, lo dimostrano le splendide superfici della Basilica di San Marco. La cattedrale venne edificate nel 1063 in una struttura già preesistente e concepita a imitazione della chiesa Santi Apostoli di Costantinopoli con alcune modifiche di ampliamento per maggiori decorazioni. L\'ispirazione dell\'arte bizantina, a Venezia, non si limita una imitazione ma al contrario si trasforma in una reinterpretazione di grande originalità. Questo scelta di rifarsi a un modello antico avviene anche per conferire all\'edificio maestosità imperiale per distinguerlo dalle semplici costruzioni coeve.<br><br>San Marco presenta una pianta a croce greca con cinque cupole rivestite completamente da na decorazioni a mosaico. Tali decorazioni si sviluppano in lungo periodo di tempo con interventi di diverse botteghe per questo lo stile bizantino utilizzato presenta delle diversità da cupola a cupola.<br><br>Se mettiamo a confronto due delle cupole più celebri quella della Pentecoste e quella dell\'Ascenzione si nota una impostazione compositiva simile ma degli approcci formali contrastanti. Nella cupola dell\'Ascenzione i personaggi sembrano più i amici e articolati, con un trattamento tortuoso delle vesti e l\'allungamento dei corpi. Nella Pentecoste gli Apostoli appaiono stauari, qui prevale lo sfondo oro che emerge come elemento autonomo e non come un semplice fondale su cui i personaggi stagnano come nell\'ascenzione. Ulteriore differenza è l\'effetto opposto generato dalle figure centrali: nella cupola dell\'Ascenzione, l\'immagine di Cristo produce un moto centrifugo che si trasmette alle figure sottostanti; invece nella Pentecoste i personaggi sembrano risucchiati dal loculo centrale, dominato dal trono dell\' entimasia.'
+};
+const Pavia = {
+    title: 'Basilica di San Michele Maggiore',
+    text: 'La Basilica di San Michele si ispira a quella di Sant’ Ambrogio a Milano, adotta infatti l’impianto basilicale e il sistema dei matronei affacciati sulla navata centrale con ampie arcate a tutto sesto. Si notano invece i contatti con l’ambiente nordeuropeo tramite lo sviluppo verticale, con l’innalzamento delle volte e aperture di finestre che conferiscono più ampia illuminazione dell’edificio. La facciata imponente ed elegante allo stesso tempo presenta delle lesene che anticipano la tripartizione delle navate interne, delle decorazioni orizzontali e una logia di coronamento e delle bifore che creano un grande effetto plastico chiaroscurale. Nel romanico fiorisce un gusto esuberante non solo nella scultura architettonica ma anche nell’arredo liturgico. Importante divenne il pulpito, il suo ruolo connesso alla proclamazione della Parola di Dio offrì infiniti spunti per gli scalpellini.'
+};
+const Bari = {
+    title: 'Basilica di San Nicola',
+    text: 'La costruzione della Basilica risale al 1087( per il volere dell\'abate Elia) e la consacrazione nel 1197. In questa chiesa le influenze lombarde le ritroviamo nella facciata con l\'impostazione a salienti e nella predilezione per la forte compattezza della struttura. Peculiare, invece, del linguaggio normanno sono i due torrioni che affiancano la facciata. All\'interno sono elementi romanici il matroneo, la cripta oratorio rialzata e triplice abside con cui si richiude il presbiterio. L\'iconostasi è invece un elemento tipicamente bizantino che separa il corpo longitudinale dal transetto. La basilica si caratterizza per la chiara definizione dei volumi e per la presenza di pieni e di vuoti. Da sottolineare sono le grandi e profonde arcate di una galleria che si apre sulle pareti laterali esterne, la quale traggono ispirazione dai modelli dell\'architettura imperiale romana.'
+};
+const Giovanni = {
+    title: 'Battistero di San Giovanni',
+    text: 'Il Battistero di San Giovanni è una delle più antiche chiese di Firenze, posto di fronte alla Cattedrale della città, la chiesa di Santa Maria del Fiore.A pianta ottagonale, interamente rivestito di lastre di marmo bianco e verde, il Battistero è coperto da una cupola ad otto spicchi poggiante sulle pareti perimetrali. Gran parte del rivestimento marmoreo del Battistero, così come i numerosi frammenti ed iscrizioni antiche e le grandi colonne che sorreggono la trabeazione sopra le porte all\'interno, provengono dalle rovine della \'Florentia\' romana, forse da qualche edificio pagano.Nel XIII secolo si dà il via anche alla decorazione musiva interna, ricoprendo la scarsella e l\'intera cupola. Entrando nell\'edificio, la prima cosa che cattura l\'attenzione è il prezioso mosaico della cupola, una delle più grandi al mondo per l\'epoca ad esser decorata con questa tecnica. I mosaici sono dominati dall\'enorme figura di Cristo giudice con scene del giudizio universale che occupano tre degli otto spicchi della cupola. Nei sovrapposti registri orizzontali degli altri cinque spicchi, sono raffigurate le storie di San Giovanni Battista, patrono di Firenze, di Cristo, di Giuseppe e della Genesi. Al centro, nel registro più in alto, trovano posto le gerarchie angeliche.<br><br>Di patronato della ricca Arte di Calimala, il Battistero venne impreziosito anche da tre bellissime porte bronzee.La più antica è la Porta Sud realizzata tra il 1330 e il 1336 dallo scultore Andrea Pisano.La Porta Nord, fu la seconda ad essere realizzata. Sostanzialmente impostata come la prima, rappresenta nelle venti formelle superiori scene del Nuovo Testamento e nelle otto formelle inferiori gli Evangelisti e i quattro Padri della Chiesa.Infine la porta est, detta da Michelangelo la Porta del Paradiso, il capolavoro oramai pienamente rinascimentale del Ghiberti e dei suoi aiuti.'
+};
+const Durham = {
+    title: 'Cattedrale di Durham',
+    text: 'La cattedrale ha una collocazione strategica che le conferisce l\'aspetto di una fortezza, proprio al centro della penisola circondata dal fiume Wear nel Nord-Est dell\'Inghilterra. La costruzione della chiesa si protrasse per un lungo periodo di tempo in cui subì diversi cambiamenti di progetto. Particolare è l\'elevazione su tre piani delle navate laterali, insomma la cattedrale di Durham viene considerata un\'opera scultorea di dimensioni monumentali: lo dimostra la raffinata decorazione geometrica delle colonne degli archivolti, producendo grandi effetti di dinamismo e alti livelli di perizia raggiunta nella lavorazione del taglio della pietra. La affascinante cattedrale rappresenta, inoltre, un modello di architettura romanica avanzata.'
+};
+const Compostela = {
+    title: 'Cattedrale di Santiago de Compostela',
+    text: 'La Cattedrale di Santiago De Compostela venne eretta nel 1075 dove doveva ospitare i resti dell\'apostolo San Giacomo Maggiore. L\'impianto a croce latina è caratterizzata da un grande transetto e da una base particolarmente ampia concepito appositamente per il flusso di Pellegrini nell\'altare maggiore. La navata centrale presenta una copertura a botte di grande effetto e ritroviamo nuovamente il matroneo aperto da bifore. Importante è luce diretta all\'incrocio del transetto che segna il significato simbolico del presbiterio e la sua centralità nel percorso di avvicinamento del fedele a Dio. Notevole anche la decorazione scultore dell\'edificio e magnifico è il programma iconografico del Portico della Gloria (ingresso occidentale della chiesa) incentrato sul Giudizio Universale sulla Gloria di Cristo in Maestà. Si evidenzia una predisposizione decisa per l\'uso delle statue colonne. Dal punto di vista stilistico vanno sottolineati sicuri contatti con l\'ambiente artistico francese, la permanenza di flussi Bizantini e il sorgere nella prima sensibilità gotica.'
+};
+const Trani = {
+    title: 'Cattedrale di Trani',
+    text: 'La Cattedrale di Trani presenta una grande originalità dovuta alla struttura su una doppia elevazione. L\'interno presenta una pianta a tre navate delineate da colonne binate; al di sopra della navata centrale si apre un matroneo con trifore di grande eleganza. L\'influenza nord-europea la ritroviamo nell\'elevazione della navata centrale su tre livelli. A Trani l\'abside presenta una grande finestra che permette di focalizzare l\'attenzione su un aspetto centrale. È interessante sottolineare il Romanico pugliese si mostra impermeabile allo sviluppo di una scuola pittorica locale, viceversa, mostra interesse nel mosaico pavimentale.'
+};
+const Modena = {
+    title: 'Duomo di Modena',
+    text: 'Il Duomo di Modena si può considerare un monumento simbolico del Romanico padano. La sua fondazione risale al 1099 e la costruzione venne affidata all\'architetto Lanfranco. La facciata si presenta in modo articolato con al presenza di pieni e di vuoti che la rendono particolarmente sensibile alla luce. La facciata è a salienti anticipando, così, la tripartizione interna del Duomo. Inoltre essa presenta una successione di trifore entro arcate cieche e un\'avanzamento del protiro, in questi modo alla facciata viene dato un effetto dinamico.<br><br>Le trifore entro arcate cieche le ritroviamo all\'interno della struttura che delimitano le aperture del matroneo, non praticabile, che affacciano verso la navata centrale. Quest\'ultima ha una complessa copertura a volta, sostituita da una precedente a capriate lignee; e anche il grandioso pontile dell\'area presbiteriale è stata una aggiunzione successiva. La grande novità del progetto fu proprio la coerenza tra gli spazi interni ed esterni e il rapporto tra architettura e decorazione scultorea si arricchisce in modo significativo.'
+};
+const Monreale = {
+    title: 'Duomo di Monreale',
+    text: 'Fra l\'interpretazione del linguaggio romanico quella più originale spicca in Sicilia con l\'influenza del mondo arabo, i contatti con la cultura Nord Europea e la persistenza dei modelli Bizantini. Dando vita a una combinazione originale e raffinata. Il cantiere del duomo di Monreale viene avviato nel 1172 dal re Normanno Guglielmo II, la sua struttura eterogenea produce ancora un effetto di splendore gli spazi grandiosi e la preziosità delle decorazioni testimoniano la destinazione regale del Monumento, quale luogo di sepoltura dinastico. La pianta presenta un corpo longitudinale a tre navate con una sorta di corpo allungato che forma un transetto complesso e si conclude con un presbiterio triabsidato, particolarmente sviluppata in altezza. Tre sono le matrici fondamentali che contribuiscono la concezione dell\'edificio:<br>- l\'impostazione dell\'architettura Normanno riconoscibile nei due Possenti Torrioni che affiancano la facciata;<br>- le decorazioni bizantine che ricoprono l\'intera superficie delle pareti interne;<br>- la delicatezza del gusto grafico dell\'arte islamica, espressa in particolare nell\'intrico di archi intrecciati a ogiva e nelle tarsie moresche delle absidi.<br><br>Le decorazioni musive vanno attribuite molto probabilmente maestranze bizantine che seppero adottare la propria prassi esecutiva allo spazio architettonico del Duomo. Le figure assumono dimensioni monumentali e sono animate da un dinamismo assai centrato. Notevole è la capacità dei mosaicisti di adattare le scene all\'andamento della superficie, ciò risulta evidente nella gigantesca immagine del Cristo Pantocratore in cui il busto ricopre l\'intera cavità della conca absidale, le braccia aperte seguono esattamente la curva del catino producendo un effetto tridimensionale e avvolgente. Nell\'episodio di Rebecca incontra i servi di Abramo il mosaicista sfrutta l\'incavo prodotto dai due archi sottostanti per rendere l\'effetto del terreno scosceso e per evidenziare lo sforzo di cammelli che protende il collo per abbeverarsi. La predominazione delle decorazioni giungono influenzare in modo significativo anche la più importante impresa scultore del complesso di Monreale: la decorazione del grande Chiostro in cui capitelli riflettono lo stesso stile conciliata delle storie bibliche e mosaico.'
+};
+const Pisa = {
+    title: 'Duomo di Pisa',
+    text: 'Il numero di Pisa venne dedicato alla vergine Assunta dopo la vittoria conseguita sulle acque di Palermo contro i Saraceni. La chiesa presenta sulla facciata 5 di logette sovrapposte mentre all\'interno le navate sono separate da imponenti colonne coronate da capitelli corinzi, alcuni provenienti dalle Terme di Caracalla a Roma. Leggendo la pianta a croce latina si possono notare le  cinque navate attraversate da un profondo transetto di cui bracci presentano un corpo basilicale absidato. Al centro del transetto si elva una cupola ellittica derivante dell\'architettura ebraica; può essere associato a modelli islamici invece lo stesso arco ogivale che congiunge la navata maggiore con l\'area presbiterale. Il magnifico grifone bronzeo di provenienza islamica che un tempo ornava il tetto della cattedrale, aveva il preciso significato di bottino di guerra da ostentare quale simbolo di vittoria evidenziando così anche l\'importanza commerciale conseguita dalla Repubblica Marinara di Pisa. Tra i motivi decorativi ricorrenti nelle pareti esterne ritroviamo l\'uso della losanga dichiara influenza orientale. Inoltre sono infinite le numerose epigrafi sulla facciata che ricordano le imprese militari che hanno visto Pisa prevalere sui nemici. Sono presenti anche elementi romanici dell\'architettura Nord Europea riconoscibili nel sistema a tre livelli del della navata centrale; sono riscontrabili inoltre derivazioni nel Romanico lombardo ad esempio nel uso della volta a crociera lungo le navate laterali sormontate da matronei.'
+};
+
+
+
+const chiese_html = [
+    document.getElementById('mont-saint-michel'), document.getElementById('sant-antimo'), document.getElementById('san-pietro-di-moissac'), document.getElementById('sant-ambrogio'), document.getElementById('san-clemente'),
+    document.getElementById('san-marco'), document.getElementById('san-michele-maggiore'), document.getElementById('san-nicola'), document.getElementById('san-giovanni'), document.getElementById('durham'),
+    document.getElementById('santiago-de-compostela'), document.getElementById('trani'), document.getElementById('modena'), document.getElementById('monreale'), document.getElementById('pisa')
+];
+
+const chiese_txt = [
+    Michel, Antimo, Moissac, Ambrogio, Clemente,
+    Venezia, Pavia, Bari, Giovanni, Durham,
+    Compostela, Trani, Modena, Monreale, Pisa
+];
+
+const chiese_img = [
+    'mont-saint-michel', 'sant-antimo', 'san-pietro-di-moissac', 'sant-ambrogio', 'san-clemente',
+    'san-marco', 'san-michele-maggiore', 'san-nicola', 'san-giovanni', 'durham',
+    'santiago-de-compostela', 'trani', 'modena', 'monreale', 'pisa'
+];
+
+for (let i = 0; i < 15; i++) {
+
+    chiese_html[i].addEventListener('click', () => {
+
+        let html_title = document.getElementById('p-title');
+        let html_body = document.getElementById('text-in');
+        let html_img = document.getElementById('image');
+
+        html_title.innerHTML = chiese_txt[i].title;
+        html_body.innerHTML = chiese_txt[i].text;
+        html_img.src = `../res/ROMANICO/${chiese_img[i]}.jpg`;
+
+    });
+
+}
